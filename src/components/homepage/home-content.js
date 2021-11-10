62c27d1aff4f8d14b8eafb241ef8e15c75aa9837
hamzahfirman <hamzahfirman08@email.arizona.edu>
2021-11-09 22:29:21 -0700

    chore: enabled MS Graph API call

--- src/components/homepage/home-content.js
+++ src/components/homepage/home-content.js
@@ -1,49 +1,63 @@
-import React from "react";
-import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
+import React, { useState } from "react";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import Button from "@mui/material/Button";
 
 import "./Home.css";
 import { findAUser } from "../../serverInterface/server";
 import HomeTemplate from "./home-template";
+import { loginRequest } from "../../authConfig";
+import { ProfileData } from "../ProfileData";
+import { callMsGraph } from "../../serverInterface/server";
 
 
- class Content extends React.Component { 
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: {}
-    }
-  }
+function ProfileContent() {
+  const { instance, accounts } = useMsal();
+  const [graphData, setGraphData] = useState(null);
 
-  componentDidMount() {
-    // let user_info= this.props.location.state;
-    // // let password = this.props.location.state.password;
-    // if(user_info){
-    //   let user_email = user_info.email
-    //   let user_pass = user_info.password
-    //   fetch(findAUser({"email": user_email, "password": user_pass}))
-    //   .then(x => this.setState({data: x})).catch(e => console.log(e));
-      
-    // }
-    // findAUser()
-  }
-  render() {
-    // if(this.state.data.length >= 1){
-    //   console.log(this.stat)
-    // }
-    
+  const name = accounts[0] && accounts[0].name;
+
+  function RequestProfileData() {
+    const request = {
+        ...loginRequest,
+        account: accounts[0]
+    };
+
+    // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+    instance.acquireTokenSilent(request).then((response) => {
+        callMsGraph(response.accessToken).then(response => setGraphData(response));
+    }).catch((e) => {
+        instance.acquireTokenPopup(request).then((response) => {
+            callMsGraph(response.accessToken).then(response => setGraphData(response));
+        });
+    });
+}
+
+  return (
+      <>
+          <h5 className="card-title">Welcome {name}</h5>
+          {graphData ? 
+               <ProfileData graphData={graphData} />
+              :
+              <Button  color="inherit" onClick={RequestProfileData}>Request Profile Information</Button>
+          }
+      </>
+  );
+};
+
+export default function Content() { 
     return (
       <>
       <HomeTemplate>
-        <UnauthenticatedTemplate>
-        <h5><center>You are not signed in! Please sign in</center></h5>
-        </UnauthenticatedTemplate>
         <AuthenticatedTemplate>
-        <h5><center>Welcome to College Biblio!</center></h5>
+          <ProfileContent />
         </AuthenticatedTemplate>
+        <UnauthenticatedTemplate>
+        <h5><center>You are not signed in! Please sign in.</center></h5>
+        </UnauthenticatedTemplate>
       </HomeTemplate>
       </>
     );
-  }}
+  }
 
-  export default Content;
+
 
--- src/serverInterface/server.js
+++ src/serverInterface/server.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { graphConfig } from "../authConfig";
 
 // const API = "https://collegebiblioapi.herokuapp.com"
 const API = "http://localhost:3001";
@@ -19,3 +20,22 @@ export const findAUser =  (data) => {
     
 }
 
+/* MS Graph API  */
+/**
+ * Attaches a given access token to a Microsoft Graph API call. Returns information about the user
+ */
+ export async function callMsGraph(accessToken) {
+   const headers = new Headers();
+   const bearer = `Bearer ${accessToken}`;
+
+   headers.append("Authorization", bearer);
+
+   const options = {
+       method: "GET",
+       headers: headers
+   };
+
+   return fetch(graphConfig.graphMeEndpoint, options)
+       .then(response => response.json())
+       .catch(error => console.log(error));
+}
