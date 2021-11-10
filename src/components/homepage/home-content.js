3de541e7187ca1a5790df3a5486b1d9112b4f173
hamzahfirman <hamzahfirman08@email.arizona.edu>
2021-11-10 14:47:20 -0700

    chore: compeleted  school email checkpoint

--- src/components/homepage/home-content.js
+++ src/components/homepage/home-content.js
@@ -6,44 +6,9 @@ import "./Home.css";
 import { findAUser } from "../../serverInterface/server";
 import HomeTemplate from "./home-template";
 import { loginRequest } from "../../authConfig";
-import { ProfileData } from "../ProfileData";
+import ProfileContent from "./profile-content";
 import { callMsGraph } from "../../serverInterface/server";
 
-
-function ProfileContent() {
-  const { instance, accounts } = useMsal();
-  const [graphData, setGraphData] = useState(null);
-
-  const name = accounts[0] && accounts[0].name;
-
-  function RequestProfileData() {
-    const request = {
-        ...loginRequest,
-        account: accounts[0]
-    };
-
-    // Silently acquires an access token which is then attached to a request for Microsoft Graph data
-    instance.acquireTokenSilent(request).then((response) => {
-        callMsGraph(response.accessToken).then(response => setGraphData(response));
-    }).catch((e) => {
-        instance.acquireTokenPopup(request).then((response) => {
-            callMsGraph(response.accessToken).then(response => setGraphData(response));
-        });
-    });
-}
-
-  return (
-      <>
-             <h5><center>Welcome, {name}</center></h5>
-          {/* {graphData ? 
-        
-              //  <ProfileData graphData={graphData} />
-              :
-              <Button  color="inherit" onClick={RequestProfileData}>Request Profile Information</Button>
-          } */}
-      </>
-  );
-};
 
 export default function Content() { 
     return (
